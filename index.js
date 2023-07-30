184e51a0aeaba2699a92eaa6a29f99daef5673b0
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-30 21:16:19 +0530

    er-diagram

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
 dotenv.config()
+
 let app = express()
 let port = process.env.PORT
 
+//modify the routes in future
+// app.get("/arbaz",(req,res)=>{
+//     res.status(400).json({message:"Arbaz backend is connected and running properly"})
+// })
 
-
-
-
-
-
-
-
-
-
-
-
-
+// app.get("/saniya",(req,res)=>{
+//     res.status(400).json({message:"saniya backend is connected and running properly"})
+// })
 
 app.listen(port, ()=>{
     console.log(`Server is accesssing on port : ${port}`);
