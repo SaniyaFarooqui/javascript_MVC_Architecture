735a070955e7cfee570bc456b6133388bd2deb82
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-08-04 22:11:00 +0530

    saved again

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,5 +2,28 @@ import user from "../models/user.js";
 
 
 class UserController{
+
+    constructor(){
+
+    }
+
+    CreateUser =async()=>{
+
+    }
     
+    UpdateUser = async()=>{
+
+    }
+
+    GetAllUser = async()=>{
+
+    }
+
+    GetUserById = async()=>{
+
+    }
+    DeleteUser = async()=>{
+        
+    }
 }
+export default UserController
